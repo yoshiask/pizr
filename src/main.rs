0580a061f0438493dbee008d13a6685e7100678b
Yoshi Askharoun <jjask7@gmail.com>
2024-12-11 02:02:21 -0600

    Fix Adapter1 service defaults?

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use zbus::{zvariant::Value, proxy, Connection};
 // Although we use `tokio` here, you can use any async runtime of choice.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let connection = Connection::session().await?;
+    let connection = Connection::system().await?;
 
     let proxy = adapter1::Adapter1Proxy::new(&connection).await?;
     let name = proxy.name().await?;
