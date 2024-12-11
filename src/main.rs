3b1314b6fdb884c4f0d2fce8005fb09fd1ae122a
Yoshi Askharoun <jjask7@gmail.com>
2024-12-10 20:00:50 -0600

    Add generated org.bluez interfaces

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[path = "../introspections/org.bluez/adapter1.rs"] mod adapter1;
+
+use std::collections::HashMap;
+use std::error::Error;
+
+use zbus::{zvariant::Value, proxy, Connection};
+
+// Although we use `tokio` here, you can use any async runtime of choice.
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let connection = Connection::session().await?;
+
+    let proxy = adapter1::Adapter1Proxy::new(&connection).await?;
+    let name = proxy.name().await?;
+    println!("{}", name);
+
+    Ok(())
 }
