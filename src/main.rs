3877914d40fba1a2ff9286945c404571baffde61
Yoshi Askharoun <jjask7@gmail.com>
2024-12-11 22:58:00 -0600

    Add more bluez interfaces

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@
 use std::collections::HashMap;
 use std::error::Error;
 
-use zbus::{zvariant::Value, proxy, Connection};
+use zbus::{zvariant::Value, proxy, Connection, fdo::ObjectManagerProxy};
 
 // Although we use `tokio` here, you can use any async runtime of choice.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let connection = Connection::system().await?;
+    let object_manager = ObjectManagerProxy::get_managed_objects();
+
 
     let proxy = adapter1::Adapter1Proxy::new(&connection).await?;
     let name = proxy.name().await?;
