1fd57e788af10491d6edf68518d76b55cc7875d0
Yoshi Askharoun <jjask7@gmail.com>
2024-12-11 23:39:06 -0600

    [WIP] List managed objects

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,27 @@
 use std::collections::HashMap;
 use std::error::Error;
 
-use zbus::{zvariant::Value, proxy, Connection, fdo::ObjectManagerProxy};
+use zbus::{zvariant::Value, proxy, Connection, fdo::ObjectManagerProxy, ObjectServer};
+use zbus::fdo::{ManagedObjects, ObjectManager};
 
 // Although we use `tokio` here, you can use any async runtime of choice.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let connection = Connection::system().await?;
-    let object_manager = ObjectManagerProxy::get_managed_objects();
-
-
-    let proxy = adapter1::Adapter1Proxy::new(&connection).await?;
-    let name = proxy.name().await?;
-    println!("{}", name);
+    let object_manager = ObjectManagerProxy::builder(&connection)
+        .path("org.bluez")?
+        .build()
+        .await?;
+    let managed_objects = object_manager.get_managed_objects().await?;
+    for object_path in managed_objects.keys() {
+        println!("Managed object: {}", object_path);
+    }
 
     Ok(())
+
+    // let proxy = adapter1::Adapter1Proxy::new(&connection).await?;
+    // let name = proxy.name().await?;
+    // println!("{}", name);
+    //
+    // Ok(())
 }
