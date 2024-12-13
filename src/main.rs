a934d178284bcefb3cb30888ae88c060ecfdb3ac
Yoshi Askharoun <jjask7@gmail.com>
2024-12-12 21:43:12 -0600

    [WIP] Build ObjectManager

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,12 @@ use zbus::fdo::{ManagedObjects, ObjectManager};
 async fn main() -> Result<(), Box<dyn Error>> {
     let connection = Connection::system().await?;
     let object_manager = ObjectManagerProxy::builder(&connection)
-        .path("org.bluez")?
+        .path("/org/bluez")?
+	.destination("org.bluez")?
         .build()
         .await?;
+    println!("Built object manager");
+
     let managed_objects = object_manager.get_managed_objects().await?;
     for object_path in managed_objects.keys() {
         println!("Managed object: {}", object_path);
