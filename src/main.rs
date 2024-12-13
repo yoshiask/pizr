dbcc06e18a4e01e72719ee873b3a5b281c2d7679
Yoshi Askharoun <jjask7@gmail.com>
2024-12-13 00:26:26 -0600

    Remove test code

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,12 @@
 use std::error::Error;
 
 use zbus::{Connection};
-use zbus::fdo::{IntrospectableProxy};
 use crate::bluez::{BLUEZ_PATH_ROOT, BLUEZ_SERVICE};
 
 // Although we use `tokio` here, you can use any async runtime of choice.
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let connection = Connection::system().await?;
-
-    let bluez_intro = IntrospectableProxy::builder(&connection)
-	    .destination(BLUEZ_SERVICE)?
-        .path(BLUEZ_PATH_ROOT)?
-        .build()
-        .await?;
-    println!("Built bluez");
-
-    let bluez_introspection = bluez_intro.introspect().await?;
-    println!("Introspection:");
-    println!("{}", bluez_introspection);
-    println!();
 
     let hci0 = adapter1::Adapter1Proxy::builder(&connection)
         .destination(BLUEZ_SERVICE)?
