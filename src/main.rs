cf23993941da6da85083e93823b4514595ddfb81
Yoshi Askharoun <jjask7@gmail.com>
2024-12-13 00:50:47 -0600

    Better device management logic

--- src/main.rs
+++ src/main.rs
@@ -28,13 +28,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
             println!("Successfully powered hci0");
         }
         else {
-            return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Failed power on hci0")));
+            return Err(Box::from("Failed power on hci0"));
         }
     }
 
-    // Make device discoverable
     let controller_discoverable = hci0.discoverable().await?;
     if !controller_discoverable {
+        // Make device discoverable
         hci0.set_discoverable(true).await?;
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
 
@@ -43,7 +43,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             println!("pizr is discoverable as '{}'", controller_name);
         }
         else {
-            return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Failed make hci0 discoverable")));
+            return Err(Box::from("Failed make hci0 discoverable"));
         }
     }
 
