15a62fa163bba1323559fba52764b6fc51f666fd
Joshua Askharoun <jjask7@gmail.com>
2025-12-28 04:11:50 -0600

    Add more BlueZ D-Bus projections

--- introspections/org.bluez/bluez.rs
+++ introspections/org.bluez/bluez.rs
@@ -1,2 +1,5 @@
 ﻿pub const BLUEZ_SERVICE: &'static str = "org.bluez";
 pub const BLUEZ_PATH_ROOT: &'static str = "/org/bluez";
+
+pub const BLUEZ_OBEX_SERVICE: &'static str = "org.bluez.obex";
+pub const BLUEZ_OBEX_PATH_ROOT: &'static str = "/org/bluez/obex";
